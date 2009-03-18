5c88ca5cf4f9a125a24c95999c3fd8f8bd5dd5c2
Jonathan Wilkins <jwilkins@bitland.net>
2009-03-17 17:06:55 -0700

    merged changes from polyclef

--- deploy.rb
+++ deploy.rb
@@ -1,32 +1,21 @@
 set :application, "myblog"
 set :user, 'username'
-set :repository,  "git://github.com/<username>/sbg.git"
-set :scm, :git
-set :checkout, "export"
-set :deploy_via, :copy
-set :branch, "master"
+set :host, 'your.webserver.hostname'
+set :host_path, '/www/#{application}/'
 
-set :copy_cache, true
-set :copy_exclude, ".git/*"
-#set :copy_cache, "/u/caches/#{application}"
-
-# require a remote file's existence
-#depend :remote, "/etc/syslog.conf"
-
-set :base_path, "/www"
-set :deploy_to, "/www/#{application}"
-
-role :app,     "hostname"
-role :web,     "hostname"
-role :db,      "hostname", :primary => true
-
-# TODO: override deploy tasks to point to update_content
+# From: http://wiki.capify.org/index.php/Neutralise_Capistrano_Default_Methods
+namespace :deploy do
+  [:setup, :update, :update_code, :finalize_update, :symlink, :restart].each do |default_task|
+    task default_task do 
+      # Neutralize default tasks
+    end
+  end
+end
 
 desc "Use rsync to transfer updated content to the server."
-task :update_content do
-  username = 'username' # user || ENV['USER']
-  server = 'hostname'
-  content_release_path = '/www/madscience/'
-  `rsync -avzL -e ssh "./htdocs" "#{username}@#{server}:#{content_release_path}" --exclude ".git"`
+namespace :deploy do
+  task :default do
+    `rsync -rvztL -e ssh "htdocs/" "#{user}@#{host}:#{host_path}" --exclude ".git"`
+  end
 end
 
